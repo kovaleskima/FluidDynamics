07cbb1fe1c3c8902aa8d2c370b5c5817ac391ccb
Maddy <madeline.kovaleski@gmail.com>
2024-10-07 14:34:11 -0700

    completed code

--- HW1.py
+++ HW1.py
@@ -4,19 +4,26 @@ import matplotlib.pyplot as plt
 
 #First, import and read csv data 
 data = pd.read_csv('lab1-water-spout-measurements.csv')
+data.reset_index(drop=True, inplace=True)
 data = data.dropna(subset=["height (cm)"])
-data.reset_index(drop=True)
+print(data)
 
-# Fit a quadratic curve to the data
+#Fit a quadratic curve to the data
 coefficients = np.polyfit(data['seconds'], data['height (cm)'], 2)
 quadratic_function = np.poly1d(coefficients)
 
-# Create a new column in the DataFrame with the fitted values
-data['y_fit'] = np.poly1d(data['seconds'].values, data['height (cm)'].values)
+#Extract leading coefficient so that we can calculate our radius
+leading_coefficient = coefficients[0]
+print("Leading coefficient a = ", leading_coefficient)
 
-# Plot the original data and the fitted curve
-plt.scatter(data['seconds'], data['height (cm)'], label='Lab Data')
-plt.plot(data['seconds'], data['y_fit'], label='Quadratic Fit', color='red')
+#Create a new column in data containing the fitted values
+data['y_fit'] = quadratic_function(data['seconds'])
+
+#Plot the original data and the fitted curve
+plt.scatter(data['seconds'], data['height (cm)'], label='Observed h(t)')
+plt.plot(data['seconds'], data['y_fit'], label='Predicted h(t)', color='red')
+plt.xlabel("time in seconds")
+plt.ylabel("height in cm")
 plt.legend()
 plt.show()
 
